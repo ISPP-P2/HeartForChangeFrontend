e2192e444cc5c7a6ab1b9d86956e0fcd05a8661e
mariorey <80252232+mariorey@users.noreply.github.com>
2023-03-05 19:58:10 +0100

    Added activities listing
    Added some activities to test the feature

--- src/routes/authorized/AuthorizedRoutes.jsx
+++ src/routes/authorized/AuthorizedRoutes.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Volunteers from '../Volunteers'
 import Dashboard from './dashboard/Dashboard'
+import Activities from '../Activities'
+
 
 
 export default function AutorizedRoutes() {
   return (
     <Routes>
         <Route path='*' element={<Dashboard />}/>
-        <Route path={'/volunteers'} element={<Volunteers/>}/>
+        <Route path={'/voluntarios'} element={<Volunteers/>}/>
+        <Route path={'/actividades'} element={<Activities/>}/>
     </Routes>
   )
 }
---
+++ src/static/activity.jsx
@@ -0,0 +1,53 @@
+
+export class CustomList {
+    constructor(list = null){
+      this.list = list
+    }
+
+    parseToTable(header = null, information = null, headerInformation = null, details = null){
+        if(header === null && information === null){
+          console.log("faltan cosas")
+          return null
+        }
+
+        let list = this.list.map((value)=> {
+          let informationParsed = information.map((info, i)=> {
+              return value[info]
+          })
+          if(details !== null && headerInformation !== null){
+            let detailsParsed = details.map((info, i)=> {
+              return value[info]
+            })
+            let aux = {
+              header: headerInformation,
+              items: detailsParsed
+            }
+            informationParsed.push(aux)
+          }
+          return informationParsed
+        })
+        return {
+            header: header,
+            items: list,
+            details: headerInformation !== null && details !== null
+        }
+    }
+
+
+}
+
+
+class Activity {
+    constructor(id, name, description, type, location,capacity,date) {
+      this.id = id;
+      this.name = name;
+      this.description = description;
+      this.type = type;
+      this.location = location;
+      this.capacity = capacity;
+      this.date = date;
+    }
+
+  }
+
+  export default users;
