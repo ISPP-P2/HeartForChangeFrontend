2f7f9754d95c2c0ac137dc7fb1b11adb7ba9c87b
Pabgiralv <giraldillo99@gmail.com>
2023-03-08 22:20:58 +0100

    CustomCardMini added on Dashboard

---
+++ src/components/CustomCardMini.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import { Box, CardContent, CardHeader, IconButton, Typography, useMediaQuery } from '@mui/material';
+
+
+
+function CustomCardMini({
+  title = 'Cosas',
+  iconD,
+  totalNumber}) {
+  const mobile = useMediaQuery('(min-width:600px)')
+
+  return (  
+    <Card sx={{
+      width: "22em",
+      height: "10em",
+      filter: "drop-shadow(1px 1px 1px rgba(0,0,0,0.3))",
+    }}>
+      <Box sx={{ 
+        padding: "0.75rem",
+        display: 'flex', 
+        flexDirection: 'row', 
+        textAlign: 'center', 
+        justifyContent: "space-between", 
+        gap: "1rem"
+        }}>
+        <Box sx={{ display: 'flex',
+         flexDirection: 'column', 
+         textAlign: 'center', 
+         gap: "1rem",
+         alignItems: "end"
+         }}>
+          <CardContent sx={{ flex: '1 0 auto' }}>
+            <Box sx={{
+              display: "flex",
+              alignItems: "flex-start",
+              flexDirection: "column",
+              gap: "0.75rem"
+            }}>
+            <Typography component="div" fontSize={"1rem"} fontWeight={220} color={"#C6C6CD"} >
+              {title}
+            </Typography>
+            <Typography component="div" fontSize={"1.75rem"} fontWeight={550}>
+              {totalNumber}
+            </Typography>
+            </Box>
+          </CardContent>  
+        </Box>
+        <Box  sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          "& .MuiSvgIcon-root": {
+            width: mobile ? "3rem" : "0rem",
+            height:  mobile ? "3rem" : "0rem",
+          }
+        }}>
+            {iconD}
+        </Box>
+      </Box>
+    </Card>
+  )
+}
+
+export default CustomCardMini
